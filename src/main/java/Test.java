ce06fa69ae0d35e4417ac10b17432b60eacc64eb
knowakowski <knowakowski@holidaycheck.com>
2017-02-27 16:13:04 +0100

    ready

--- src/main/java/Test.java
+++ src/main/java/Test.java
@@ -1,35 +1,35 @@
-
-import org.w3c.dom.Document;
-import org.xml.sax.SAXException;
-
-import javax.xml.parsers.DocumentBuilder;
-import javax.xml.parsers.DocumentBuilderFactory;
-import javax.xml.parsers.ParserConfigurationException;
 import java.io.*;
 import java.net.URL;
 
 public class Test {
-    private static String sitemapsFolder = "google_sitemaps";
+    private static final String urlString = "http://0.0.0.0:7070/sitemap?lang=de";
+
+    private static final String sitemapsFolder = "google_sitemaps";
+
+    private static final File oldIndexFile = new File(new File(sitemapsFolder), "ch_index.xml");
+    private static final File newIndexFile = new File(new File(sitemapsFolder), "index.xml");
+
 
     public static void main(String[] args) {
-
-//        try{
-//            URL url = new URL("http://0.0.0.0:7070/sitemap?lang=de");
-//            File file = new File(sitemapsFolder);
-//            ArchiveUtils.unpackArchive(url, file);
-//
-//
-//        } catch(IOException x) {
-//            x.getStackTrace();
-//        }
-
-
-        File newIndexFile = new File(new File(sitemapsFolder), "index.xml");
-        File oldIndexFile = new File(new File(sitemapsFolder), "ch_index.xml");
-
-        XmlUtils.copySitemaps(oldIndexFile, newIndexFile);
 
     }
 
 
+    public static void downloadFile() {
+        try{
+            URL url = new URL(urlString);
+            File file = new File(sitemapsFolder);
+
+            ArchiveUtils.unpackArchive(url, file);
+
+        } catch(IOException x) {
+            x.getStackTrace();
+        }
+    }
+
+
+    public static void updateIndexFile() {
+        XmlUtils.copySitemaps(oldIndexFile, newIndexFile);
+        newIndexFile.delete();
+    }
 }
