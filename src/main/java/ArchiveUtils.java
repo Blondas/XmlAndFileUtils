f8f8ab4db3dd2409319cb88a8394aac42cc8aba8
gustawx <skustosz@gmail.com>
2017-02-27 22:17:42 +0100

    zrobilem kilka kosmetycznych zmian XmlUtils ale nie wiem do konca co to ma robic, np dwie metody sa nieuzywane nie wiem czy do usuniecia czy beeda do czegos innego. W metodzie copySitemaps tworzysz obiekt sitemapindex ktorego w ogole nie uzywasz wiec de facto wykonuje sie tam tylko transformata czyli wszystko przed nia moze zostac usuniete. Moze zadzwon rano, tylko nie za wczesnie :)

--- src/main/java/ArchiveUtils.java
+++ src/main/java/ArchiveUtils.java
@@ -1,76 +1,24 @@
+import net.lingala.zip4j.core.ZipFile;
+import lombok.SneakyThrows;
+import org.apache.commons.io.FileUtils;
+
 import java.io.*;
 import java.net.URL;
-import java.util.Enumeration;
-import java.util.zip.ZipEntry;
-import java.util.zip.ZipFile;
 
 public class ArchiveUtils {
 
-    public static void unpackArchive(URL url, File targetDir) throws IOException {
-        if (!targetDir.exists()) {
-            targetDir.mkdirs();
-        }
-
-        InputStream in = new BufferedInputStream(url.openStream(), 1024);
-
+    @SneakyThrows
+    public static void unpackArchive(URL url, File targetDir) {
         // make sure we get the actual file
         File zip = File.createTempFile("arc", ".zip", targetDir);
-        OutputStream out = new BufferedOutputStream(new FileOutputStream(zip));
-        copyInputStream(in, out);
-        out.close();
-
+        zip.deleteOnExit();
+        FileUtils.copyURLToFile(url, zip);
         unpackArchive(zip, targetDir);
-
-        zip.delete();
     }
 
-    public static File unpackArchive(File zippedFile, File targetDir) throws IOException {
-        if (!zippedFile.exists()) {
-            throw new IOException(zippedFile.getAbsolutePath() + " does not exist");
-        }
-        if (!buildDirectory(targetDir)) {
-            throw new IOException("Could not create directory: " + targetDir);
-        }
-
+    @SneakyThrows
+    public static void unpackArchive(File zippedFile, File targetDir) {
         ZipFile zipFile = new ZipFile(zippedFile);
-        for (Enumeration entries = zipFile.entries(); entries.hasMoreElements();) {
-            ZipEntry entry = (ZipEntry) entries.nextElement();
-            File file = new File(targetDir, File.separator + entry.getName());
-
-            if (!buildDirectory(file.getParentFile())) {
-                throw new IOException("Could not create directory: " + file.getParentFile());
-            }
-
-            if (!entry.isDirectory()) {
-                copyInputStream(zipFile.getInputStream(entry), new BufferedOutputStream(new FileOutputStream(file)));
-            } else {
-                if (!buildDirectory(file)) {
-                    throw new IOException("Could not create directory: " + file);
-                }
-            }
-        }
-
-        zipFile.close();
-
-        return zippedFile;
+        zipFile.extractAll(targetDir.getPath());
     }
-
-    private static void copyInputStream(InputStream in, OutputStream out) throws IOException {
-        byte[] buffer = new byte[1024];
-        int len = in.read(buffer);
-
-        while (len >= 0) {
-            out.write(buffer, 0, len);
-            len = in.read(buffer);
-        }
-
-        in.close();
-        out.close();
-    }
-
-
-    private static boolean buildDirectory(File file) {
-        return file.exists() || file.mkdirs();
-    }
-
 }
--- src/main/java/XmlUtils.java
+++ src/main/java/XmlUtils.java
@@ -1,16 +1,11 @@
+import lombok.SneakyThrows;
 import org.w3c.dom.*;
-import org.xml.sax.SAXException;
 
-import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
-import javax.xml.parsers.ParserConfigurationException;
-import javax.xml.transform.TransformerException;
 import javax.xml.transform.TransformerFactory;
 import javax.xml.transform.dom.DOMSource;
 import javax.xml.transform.stream.StreamResult;
 import java.io.File;
-import java.io.IOException;
-
 
 public class XmlUtils {
 
@@ -24,63 +19,33 @@ public class XmlUtils {
         return dom;
     }
 
-    private static NodeList findElements(File file, String tagName)
-            throws ParserConfigurationException, IOException, SAXException{
-
+    private static NodeList findElements(File file, String tagName){
         Document doc = parseXmlFile(file);
         return doc.getDocumentElement().getElementsByTagName(tagName);
     }
-
-    private static Document parseXmlFile(File file) throws ParserConfigurationException, IOException, SAXException{
-        DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
-        DocumentBuilder db = dbf.newDocumentBuilder();
-
-        return db.parse(file);
+    @SneakyThrows
+    private static Document parseXmlFile(File file) {
+        return DocumentBuilderFactory.newInstance()
+                                     .newDocumentBuilder()
+                                     .parse(file);
     }
-
-    public static void addSitemaps(File from, File to) {
-        try {
-            NodeList sitemaps = findElements(from, "sitemap");
-            NodeList sitemapindex = findElements(to, "sitemapindex");
-        } catch (ParserConfigurationException pce) {
-            pce.printStackTrace();
-        } catch (IOException ioe) {
-            ioe.printStackTrace();
-        } catch (SAXException sae) {
-            sae.printStackTrace();
-        }
-
-
-    }
-
+    @SneakyThrows
     public static void copySitemaps(File oldSitemapFile, File newSitemapFile) {
-        try {
-            Document oldSitemaps = parseXmlFile(oldSitemapFile);
-            Document newSitemaps = parseXmlFile(newSitemapFile);
+        Document oldSitemapsDocument = parseXmlFile(oldSitemapFile);
+        Document newSitemapsDocument = parseXmlFile(newSitemapFile);
+        NodeList newSitemapsNodes = newSitemapsDocument.getDocumentElement().getElementsByTagName("sitemap");
 
-            NodeList sitemaps = newSitemaps.getDocumentElement().getElementsByTagName("sitemap");
+//      addNodes(oldSitemaps.getDocumentElement(), sitemaps);
 
-//            addNodes(oldSitemaps.getDocumentElement(), sitemaps);
-
-            Element sitemapindex = oldSitemaps.getDocumentElement();
-            for (int i = 0; i < sitemaps.getLength(); i++) {
-                Node nodeImported = oldSitemaps.importNode(sitemaps.item(i), true);
-                sitemapindex.appendChild(nodeImported);
-            }
-
-            TransformerFactory
-                    .newInstance()
-                    .newTransformer()
-                    .transform(new DOMSource(oldSitemaps), new StreamResult(oldSitemapFile));
-
-        } catch (ParserConfigurationException pce) {
-            pce.printStackTrace();
-        } catch (TransformerException tfe) {
-            tfe.printStackTrace();
-        } catch (IOException ioe) {
-            ioe.printStackTrace();
-        } catch (SAXException sae) {
-            sae.printStackTrace();
+        Element sitemapindex = oldSitemapsDocument.getDocumentElement();
+        for (int i = 0; i < newSitemapsNodes.getLength(); i++) {
+            Node nodeImported = oldSitemapsDocument.importNode(newSitemapsNodes.item(i), true);
+            sitemapindex.appendChild(nodeImported);
         }
+
+        TransformerFactory
+                .newInstance()
+                .newTransformer()
+                .transform(new DOMSource(oldSitemapsDocument), new StreamResult(oldSitemapFile));
     }
 }
