7c99ed330002f1901e7fefd8157181d747f77547
knowakowski <knowakowski@holidaycheck.com>
2017-02-27 14:37:40 +0100

    ArchiveUtils

--- src/main/java/Test.java
+++ src/main/java/Test.java
@@ -6,25 +6,26 @@ import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.ParserConfigurationException;
 import java.io.*;
+import java.net.URL;
 
 public class Test {
     private static String sitemapsFolder = "google_sitemaps";
 
     public static void main(String[] args) {
 
-//        try{
-//            URL url = new URL("http://0.0.0.0:7070/sitemap?lang=de");
-//            File file = new File(sitemapsFolder);
-//            ArchiveUtils.unpackArchive(url, file);
-//
-//
-//        } catch(IOException x) {
-//            x.getStackTrace();
-//        }
+        try{
+            URL url = new URL("http://0.0.0.0:7070/sitemap?lang=de");
+            File file = new File(sitemapsFolder);
+            ArchiveUtils.unpackArchive(url, file);
 
 
-        File newIndexFile = new File(new File(sitemapsFolder), "index.xml");
-        XmlUtils.dupa(newIndexFile);
+        } catch(IOException x) {
+            x.getStackTrace();
+        }
+
+
+//        File newIndexFile = new File(new File(sitemapsFolder), "index.xml");
+//        XmlUtils.dupa(newIndexFile);
 
     }
 
--- src/main/java/XmlUtils.java
+++ src/main/java/XmlUtils.java
@@ -8,41 +8,41 @@ import java.io.File;
 import java.io.IOException;
 
 
-public class XmlUtils {
-
-    public static void addNode(String tagName, String value, Node parent) {
-        Document dom = parent.getOwnerDocument();
-
-        // Create a new Node with the given tag name
-        Node node = dom.createElement(tagName);
-
-        // Add the node value as a child text node
-        Text nodeVal = dom.createTextNode(value);
-        Node c = node.appendChild(nodeVal);
-
-        // Add the new node structure to the parent node
-        parent.appendChild(node);
-    }
-
-    public static NodeList findElements(File file, String tagName) {
-        Document doc = parseXmlFile(file);
-
-        return doc.getDocumentElement().getElementsByTagName(tagName)
-    }
-
-    public static Document parseXmlFile(File file) {
-        try {
-            DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
-            DocumentBuilder db = dbf.newDocumentBuilder();
-
-            return db.parse(file);
-        } catch (ParserConfigurationException ex) {
-            ex.printStackTrace();
-        } catch (IOException ex) {
-            ex.printStackTrace();
-        } catch (SAXException ex) {
-            ex.printStackTrace();
-        }
-
-    }
-}
+//public class XmlUtils {
+//
+//    public static void addNode(String tagName, String value, Node parent) {
+//        Document dom = parent.getOwnerDocument();
+//
+//        // Create a new Node with the given tag name
+//        Node node = dom.createElement(tagName);
+//
+//        // Add the node value as a child text node
+//        Text nodeVal = dom.createTextNode(value);
+//        Node c = node.appendChild(nodeVal);
+//
+//        // Add the new node structure to the parent node
+//        parent.appendChild(node);
+//    }
+//
+//    public static NodeList findElements(File file, String tagName) {
+//        Document doc = parseXmlFile(file);
+//
+//        return doc.getDocumentElement().getElementsByTagName(tagName)
+//    }
+//
+//    public static Document parseXmlFile(File file) {
+//        try {
+//            DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
+//            DocumentBuilder db = dbf.newDocumentBuilder();
+//
+//            return db.parse(file);
+//        } catch (ParserConfigurationException ex) {
+//            ex.printStackTrace();
+//        } catch (IOException ex) {
+//            ex.printStackTrace();
+//        } catch (SAXException ex) {
+//            ex.printStackTrace();
+//        }
+//
+//    }
+//}
